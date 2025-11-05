de9c3c156fb307ad63e9c2182e989d0380c96910
kishanshettyb <kishanqr@gmail.com>
2025-11-05 17:48:53 +0530

    fix issues

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,24 @@
+import { useIsFocused } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Platform, StatusBar as RNStatusBar, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { WebView } from "react-native-webview";
 
 export default function App() {
+	const isFocused = useIsFocused();
+	const [webViewKey, setWebViewKey] = useState(0);
+
+	useEffect(() => {
+		if (isFocused) {
+			// Update key to force WebView reload every time tab is focused
+			setWebViewKey((prevKey) => prevKey + 1);
+		}
+	}, [isFocused]);
 	return (
 		<SafeAreaView style={[styles.container, { paddingTop: Platform.OS === "android" ? RNStatusBar.currentHeight : 0 }]} edges={["left", "right", "bottom"]}>
 			<StatusBar style="dark" />
-			<WebView source={{ uri: "https://college-project-nu-silk.vercel.app" }} style={styles.webview} />
+			<WebView key={webViewKey} source={{ uri: "https://college-project-nu-silk.vercel.app" }} style={styles.webview} />
 		</SafeAreaView>
 	);
 }
