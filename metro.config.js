28727c51955bd3ef41ddb5aca56a6367b9767bec
kishanshettyb <kishanqr@gmail.com>
2025-11-27 11:55:42 +0530

    fix issues

--- metro.config.js
+++ metro.config.js
@@ -2,18 +2,6 @@ const { getDefaultConfig } = require("@expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-// If you added custom settings (images, SVG, etc.), modify *config* here
-// Example for SVG support:
-// const { mergeConfig } = require("metro-config");
-// const svgConfig = {
-//   transformer: {
-//     babelTransformerPath: require.resolve("react-native-svg-transformer"),
-//   },
-//   resolver: {
-//     assetExts: config.resolver.assetExts.filter((ext) => ext !== "svg"),
-//     sourceExts: [...config.resolver.sourceExts, "svg"],
-//   },
-// };
-// module.exports = mergeConfig(config, svgConfig);
+// add your custom edits here
 
 module.exports = config;
